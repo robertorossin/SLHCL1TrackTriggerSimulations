1c39afe5d00444cd2cb114f9b0e78760b9773dc0
Jia Fu Low <jia.fu.low@cern.ch>
2014-04-16 10:57:46 -0500

    Use pt 1-100, and 1/pt 0.01-1
    	renamed:    SingleMuOneOverPt_0005_05_cfi.py -> SingleMuPlusMinusFlatOneOverPt0p01To1_cfi.py
    	renamed:    SingleMuPt_2_200_cfi.py -> SingleMuPlusMinusFlatPt1To100_cfi.py

--- python/SingleMuPlusMinusFlatOneOverPt0p01To1_cfi.py
+++ python/SingleMuPlusMinusFlatOneOverPt0p01To1_cfi.py
@@ -1,13 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
-generator = cms.EDProducer("FlatRandomPtGunProducer",
+# reference: https://github.com/cms-sw/genproductions/blob/master/python/EightTeV/SingleMuMinusFlatPt0p2To100_cff.py
+generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
     PGunParameters = cms.PSet(
-        MaxPt = cms.double(200.0),
-        MinPt = cms.double(2.0),
+        MaxOneOverPt = cms.double(1),
+        MinOneOverPt = cms.double(0.01),
         PartID = cms.vint32(-13),
-        MaxEta = cms.double(2.5),
+        MaxEta = cms.double(3),
         MaxPhi = cms.double(3.14159265359),
-        MinEta = cms.double(-2.5),
+        MinEta = cms.double(-3),
         MinPhi = cms.double(-3.14159265359), ## in radians
         XFlatSpread = cms.double(1.5), ## in mm
         YFlatSpread = cms.double(1.5), ## in mm
@@ -15,7 +16,7 @@ generator = cms.EDProducer("FlatRandomPtGunProducer",
     ),
     Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
 
-    psethack = cms.string('single mu+- pt 2-200'),
+    psethack = cms.string('single mu+- 1/pt 0.01 to 1'),
     AddAntiParticle = cms.bool(True),
     firstRun = cms.untracked.uint32(1)
 )
--- python/SingleMuPlusMinusFlatPt1To100_cfi.py
+++ python/SingleMuPlusMinusFlatPt1To100_cfi.py
@@ -1,21 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
-generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
+# reference: https://github.com/cms-sw/genproductions/blob/master/python/EightTeV/SingleMuMinusFlatPt0p2To100_cff.py
+generator = cms.EDProducer("FlatRandomPtGunProducer",
     PGunParameters = cms.PSet(
-        MaxOneOverPt = cms.double(0.5),
-        MinOneOverPt = cms.double(0.005),
+        MaxPt = cms.double(100.0),
+        MinPt = cms.double(1.0),
         PartID = cms.vint32(-13),
-        MaxEta = cms.double(2.5),
+        MaxEta = cms.double(3),
         MaxPhi = cms.double(3.14159265359),
-        MinEta = cms.double(-2.5),
+        MinEta = cms.double(-3),
         MinPhi = cms.double(-3.14159265359), ## in radians
         XFlatSpread = cms.double(1.5), ## in mm
         YFlatSpread = cms.double(1.5), ## in mm
         ZFlatSpread = cms.double(150.), ## in mm
     ),
     Verbosity = cms.untracked.int32(0), ## set to 1 (or greater)  for printouts
-
-    psethack = cms.string('single mu+- 1/pt 0.005-0.5'),
+    psethack = cms.string('single mu+- pt 1 to 100'),
     AddAntiParticle = cms.bool(True),
     firstRun = cms.untracked.uint32(1)
 )
