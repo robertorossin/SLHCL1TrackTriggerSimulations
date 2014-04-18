ca41d179519bd7984cbb32dd133cbd46947935f1
Jia Fu Low <jia.fu.low@cern.ch>
2014-04-18 09:14:40 -0500

    Apply DataProduction --> SLHCL1TrackTriggerSimulations renaming.

--- python/MIB_extractor_cff.py
+++ python/MIB_extractor_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.MagneticField_cff import *
-from Extractors.RecoExtractor.MIB_extractor_cfi import *
+from SLHCL1TrackTriggerSimulations.RecoExtractor.MIB_extractor_cfi import *
 
