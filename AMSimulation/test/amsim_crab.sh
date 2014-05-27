0bd31a056978cce747003bf2a099b710a8fa70ad
Jia Fu Low <jia.fu.low@cern.ch>
2014-05-27 09:15:37 -0500

    Updated batch scripts

--- AMSimulation/test/amsim_crab.sh
+++ AMSimulation/test/amsim_crab.sh
@@ -29,23 +29,42 @@ fi
 args=("${@:2}")
 
 # Rename
-if ls input_*.txt > /dev/null 2>&1; then
+#echo "Files in current working directory (before rename): "
+#ls .
+if  ls input_*.txt > /dev/null 2>&1; then
     mv input_*.txt input.txt
 fi
-if ls patternBank_*.root > /dev/null 2>&1; then
-    mv patternBank_*.root patternBank.root
+if  ls link_patternBank_*.sh > /dev/null 2>&1; then
+    mv link_patternBank_*.sh link_patternBank.sh
+    chmod a+x link_patternBank.sh
 fi
+#echo "Files in current working directory (after rename): "
+#ls .
+
 
 # Take line i from input.txt
 sed -i "$i"'q;d' input.txt
 
-echo "Running on input.txt:"
+echo "Running on input source:"
 cat input.txt
 echo "  max events: $n"
 echo "  arguments: ${args[@]}"
 
-./amsim.exe -R -i input.txt -o roads.root -B patternBank.root --bank_nDCBits 0 -n "$n" "${args[@]}" --no-color >& amsimR.log
+echo "Using pattern bank:"
+cat link_patternBank.sh
+./link_patternBank.sh
+
+./amsim.exe -R -i input.txt -o roads.root -B patternBank.root -n "$n" "${args[@]}" --no-color >& amsimR.log
 ./amsim.exe -F -i roads.root -o tracks.root -n "$n" "${args[@]}" --no-color >& amsimF.log
 ./amsim.exe -W -i input.txt -o results.root --roads roads.root --tracks tracks.root -n "$n" "${args[@]}" --no-color >& amsimW.log
 cat amsimR.log amsimF.log amsimW.log
+
+echo "<FrameworkJobReport>
+	<ExitCode Value=\"0\"/>
+	<TimingService/>
+	<PerformanceReport>
+		<CPU/>
+		<Memory/>
+	</PerformanceReport>
+</FrameworkJobReport>" > ../crab_fjr_$NJob.xml
 
