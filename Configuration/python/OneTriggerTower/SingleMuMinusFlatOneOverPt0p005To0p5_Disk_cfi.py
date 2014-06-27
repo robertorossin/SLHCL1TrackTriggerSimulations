c146235a2ec21ea21997a3a9e7af2ddf69d67549
Jia Fu Low <jia.fu.low@cern.ch>
2014-06-27 13:36:04 -0500

    Added SingleMu fragments for one single trigger tower

---
+++ Configuration/python/OneTriggerTower/SingleMuMinusFlatOneOverPt0p005To0p5_Disk_cfi.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+
+# reference: https://github.com/cms-sw/genproductions/blob/master/python/EightTeV/SingleMuMinusFlatPt0p2To100_cff.py
+generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
+    PGunParameters = cms.PSet(
+        MaxOneOverPt = cms.double(0.5),
+        MinOneOverPt = cms.double(0.005),
+        PartID = cms.vint32(13),
+        MaxEta = cms.double(2.5),
+        MaxPhi = cms.double(1.6),
+        MinEta = cms.double(1.3),
+        MinPhi = cms.double(0.6),
+        XFlatSpread = cms.double(1.5), ## in mm
+        YFlatSpread = cms.double(1.5), ## in mm
+        ZFlatSpread = cms.double(150.), ## in mm
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('single mu- 1/pt 0.005 to 0.5'),
+    AddAntiParticle = cms.bool(False),
+    firstRun = cms.untracked.uint32(1)
+)
