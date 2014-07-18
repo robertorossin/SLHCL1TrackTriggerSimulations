a2ebb1e245511bc3cc4882d25e4e62f3952e4afc
Jia Fu Low <jia.fu.low@cern.ch>
2014-07-18 01:20:03 -0500

    Enabled timing printout in tests.

--- AMSimulation/test/runtests_amsim.sh
+++ AMSimulation/test/runtests_amsim.sh
@@ -10,18 +10,18 @@ PYTHONTEST=${CMSSW_BASE}/src/SLHCL1TrackTriggerSimulations/AMSimulation/python/t
 
 #(amsim --help) || die 'Failure getting help message' $?
 
-(amsim -C -i test_ntuple.txt -o stubs.root -n 100 --no-filter) || die 'Failure during stub cleaning' $?
+(amsim -C -i test_ntuple.txt -o stubs.root -n 100 --no-filter --timing) || die 'Failure during stub cleaning' $?
 (python ${PYTHONTEST}/testStubCleaning.py ${LOCAL_TOP_DIR}/stubs.root) || die 'Failure using tesStubCleaning.py' $?
 
-(amsim -G -i stubs.root -o patternBank.root -L trigger_sector_map.csv -n 100 --bank_nDCBits 0) || die 'Failure during pattern bank generation' $?
+(amsim -G -i stubs.root -o patternBank.root -L trigger_sector_map.csv -n 100 --bank_nDCBits 0 --timing) || die 'Failure during pattern bank generation' $?
 (python ${PYTHONTEST}/testPatternBankGeneration.py ${LOCAL_TOP_DIR}/patternBank.root) || die 'Failure using patternBankGeneration.py' $?
 
-(amsim -R -i test_ntuple.txt -o roads.root -B patternBank.root -n 100 --bank_nDCBits 0) || die 'Failure during pattern recognition' $?
+(amsim -R -i test_ntuple.txt -o roads.root -B patternBank.root -n 100 --bank_nDCBits 0 --timing) || die 'Failure during pattern recognition' $?
 (python ${PYTHONTEST}/testPatternRecognition.py ${LOCAL_TOP_DIR}/roads.root) || die 'Failure using testPatternRecognition.py' $?
 
-(amsim -F -i roads.root -o tracks.root -n 100) || die 'Failure during track fitting' $?
+(amsim -F -i roads.root -o tracks.root -n 100 --timing) || die 'Failure during track fitting' $?
 (python ${PYTHONTEST}/testTrackFitting.py ${LOCAL_TOP_DIR}/tracks.root) || die 'Failure using testTrackFitting.py' $?
 
-(amsim -W -i test_ntuple.txt -o results.root --roads roads.root --tracks tracks.root -n 100) || die 'Failure during ntuple writing' $?
+(amsim -W -i test_ntuple.txt -o results.root --roads roads.root --tracks tracks.root -n 100 --timing) || die 'Failure during ntuple writing' $?
 (python ${PYTHONTEST}/testWriting.py ${LOCAL_TOP_DIR}/results.root) || die 'Failure using testWriting.py' $?
 
