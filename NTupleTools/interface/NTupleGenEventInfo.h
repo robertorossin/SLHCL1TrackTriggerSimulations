2429019ed0e87c3f7b1589bea2dc4847cf21d44f
Jia Fu Low <jia.fu.low@cern.ch>
2014-11-18 23:07:36 -0600

    Trivial fix in NTupleGenEventInfo

--- NTupleTools/interface/NTupleGenEventInfo.h
+++ NTupleTools/interface/NTupleGenEventInfo.h
@@ -4,8 +4,6 @@
 #include "SLHCL1TrackTriggerSimulations/NTupleTools/interface/NTupleCommon.h"
 
 
-// FIXME: add random number seed for particle gun events
-
 class NTupleGenEventInfo : public edm::EDProducer {
   public:
     explicit NTupleGenEventInfo(const edm::ParameterSet&);
