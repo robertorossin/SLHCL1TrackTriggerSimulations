532113f204b69988f4bbabb07dce9997ef56d17a
Jia Fu Low <jia.fu.low@cern.ch>
2014-12-07 18:24:23 -0600

    Updated drawer_stubs.py

--- AMSimulation/res/drawer_stubs.py
+++ AMSimulation/res/drawer_stubs.py
@@ -2,19 +2,22 @@
 
 from rootdrawing import *
 from roothelper import *
+from array import array
+from math import pi, sin, cos, tan, sinh
 
 # ______________________________________________________________________________
 # Configurations
 
 sections = {}
-sections["residual"] = False
-sections["interval"] = True
+sections["residual"    ] = False
+sections["interval"    ] = False
+sections["multiplicity"] = True
 
 
 drawerInit = DrawerInit()
 
-infiles = ["/eos/uscms/store/user/l1upgrades/SLHC/GEN/620_SLHC12p1_ntuple/stubs_barrel_400M/stubs_barrel_400M.1.root"]
-#infiles = ["/eos/uscms/store/user/l1upgrades/SLHC/GEN/620_SLHC12p1_ntuple/stubs_barrel_400M/stubs_barrel_400M.2.root"]
+infiles = ["/uscms_data/d2/jiafu/L1TrackTrigger/CRAB_amsim_luciano/stubs_barrel_2M.2.root"]
+infilestxt = "/uscms_data/d2/jiafu/L1TrackTrigger/CRAB_amsim_luciano/input_bank_barrel_20141105.txt"
 
 col  = TColor.GetColor("#1f78b4")  # Paired
 fcol = TColor.GetColor("#a6cee3")
@@ -22,8 +25,8 @@ imgdir = "figures_stubs/"
 
 
 # Number of events
-nentries = 1000000
-#nentries = 10000
+nentries = 2000000
+#nentries = 2000
 
 chain = TChain("ntupler/tree", "")
 for f in infiles:
@@ -56,29 +59,29 @@ if sections["residual"]:
     cut = "@TTStubs_r.size()==6"
 
     # Barrel
-    phi_xmaxs = [0.006, 0.008, 0.010, 0.014, 0.018, 0.024]
-    z_xmaxs = [0.5,0.5,0.5,5,5,5]
+    phimaxs = [0.006, 0.008, 0.010, 0.014, 0.018, 0.024]
+    zmaxs = [0.6, 0.6, 0.6, 6, 6, 6]
     for i in xrange(5,11):
         layer = "b%i" % i
         ii = i-5
 
         var = "TTStubs_phi[%i] - (genParts_phi[0] - 0.3*3.8*TTStubs_r[%i]*1e-2 * (genParts_charge[0]/genParts_pt[0]) / 2.0)" % (ii,ii)
-        xtitle = "Stub #phi_{%s} - Track #phi_{%s,exp}" % (layer,layer)
-        p0 = struct("phiresidual_%s" % layer, var, cut, xtitle, 200, -phi_xmaxs[ii], phi_xmaxs[ii], col, fcol)
+        xtitle = "stub #phi_{%s} - ideal #phi_{%s}" % (layer,layer)
+        p0 = struct("phiresidual_%s" % layer, var, cut, xtitle, 240, -phimaxs[ii], phimaxs[ii], col, fcol)
         histos = doit([p0], imgdir)
-        print histos[0].h.GetRMS()
+        print histos[0].h.Integral(), histos[0].h.GetRMS()
 
         var = "TTStubs_z[%i] - (genParts_vz[0] + TTStubs_r[%i] * sinh(genParts_eta[0]))" % (ii,ii)  # cot(theta) = sinh(eta) = pz/pt
-        xtitle = "Stub z_{%s} - Track z_{%s,exp} [cm]" % (layer,layer)
-        p0 = struct("zresidual_%s" % layer, var, cut, xtitle, 200, -z_xmaxs[ii], z_xmaxs[ii], col, fcol)
+        xtitle = "stub z_{%s} - ideal z_{%s} [cm]" % (layer,layer)
+        p0 = struct("zresidual_%s" % layer, var, cut, xtitle, 240, -zmaxs[ii], zmaxs[ii], col, fcol)
         histos = doit([p0], imgdir)
-        print histos[0].h.GetRMS()
+        print histos[0].h.Integral(), histos[0].h.GetRMS()
 
 
 # ______________________________________________________________________________
 if sections["interval"]:
 
-    def bookLayer():
+    def bookLayer(tt):
         histos = {}
         for i in xrange(5,23):
             if i == 16 or i == 17:  continue
@@ -86,40 +89,44 @@ if sections["interval"]:
                 layer = "b%i" % i
             else:
                 layer = "e%i" % i
-            histos["phiinterval_%s" % layer] = TH1F("phiinterval_%s" % layer, "; Stub #phi_{%s}; Entries" % layer, 640*2, -3.2, 3.2)
-            histos["etainterval_%s" % layer] = TH1F("etainterval_%s" % layer, "; Stub #eta_{%s}; Entries" % layer, 600*2, -3.0, 3.0)
-            histos["zinterval_%s" % layer] = TH1F("zinterval_%s" % layer, "; Stub z_{%s} [cm]; Entries" % layer, 600*2, -300.0, 300.0)
+            h = TH1F("phiinterval_tt%i_%s" % (tt,layer), "; stub #phi_{%s}; Entries" % layer, 640*2, -3.2, 3.2)
+            histos[h.GetName()] = h
+            h = TH1F("etainterval_tt%i_%s" % (tt,layer), "; stub #eta_{%s}; Entries" % layer, 600*2, -3.0, 3.0)
+            histos[h.GetName()] = h
+            h = TH1F("xinterval_tt%i_%s" % (tt,layer), "; stub r#phi_{%s} [cm]; Entries" % layer, 600*2, -300.0, 300.0)
+            histos[h.GetName()] = h
+            h = TH1F("zinterval_tt%i_%s" % (tt,layer), "; stub z_{%s} [cm]; Entries" % layer, 600*2, -300.0, 300.0)
+            histos[h.GetName()] = h
         return histos
 
     def projectLayer(tree, histos, tt, nentries=1000000000):
         tree.SetBranchStatus("*", 0)
+        tree.SetBranchStatus("TTStubs_r"      , 1)
         tree.SetBranchStatus("TTStubs_phi"    , 1)
         tree.SetBranchStatus("TTStubs_eta"    , 1)
         tree.SetBranchStatus("TTStubs_z"      , 1)
         tree.SetBranchStatus("TTStubs_modId"  , 1)
 
-        ievt = tree
-        for i_ievt in xrange(nentries):
-            if i_ievt < nentries/2:
-                tree.GetEntry(i_ievt)
-            else:
-                tree.GetEntry(200000000+i_ievt)
+        # Loop over events
+        for i_ievt, ievt in enumerate(tree):
+            if (i_ievt == nentries):  break
 
-            for phi, eta, z, moduleId in izip(ievt.TTStubs_phi, ievt.TTStubs_eta, ievt.TTStubs_z, ievt.TTStubs_modId):
+            for r, phi, eta, z, moduleId in izip(ievt.TTStubs_r, ievt.TTStubs_phi, ievt.TTStubs_eta, ievt.TTStubs_z, ievt.TTStubs_modId):
                 if moduleId in ttmap[tt]:
                     lay = decodeLayer(moduleId)
                     if lay < 11:
                         layer = "b%i" % lay
                     else:
                         layer = "e%i" % lay
-                    histos["phiinterval_%s" % layer].Fill(phi)
-                    histos["etainterval_%s" % layer].Fill(eta)
-                    histos["zinterval_%s" % layer].Fill(z)
+                    histos["phiinterval_tt%i_%s" % (tt,layer)].Fill(phi)
+                    histos["etainterval_tt%i_%s" % (tt,layer)].Fill(eta)
+                    histos["xinterval_tt%i_%s" % (tt,layer)].Fill(r*phi)
+                    histos["zinterval_tt%i_%s" % (tt,layer)].Fill(z)
 
         tree.SetBranchStatus("*", 1)
         return
 
-    def find_xinterval(h):
+    def find_interval(h):
         nbins = h.GetNbinsX()
         xmin, xmax = 0, nbins+2
         for i in xrange(nbins+2):
@@ -133,7 +140,6 @@ if sections["interval"]:
         return xmin, xmax
 
     def drawLayer(histos, imgdir, tt):
-
         for i in xrange(5,23):
             if i == 16 or i == 17:  continue
             if i < 11:
@@ -141,53 +147,220 @@ if sections["interval"]:
             else:
                 layer = "e%i" % i
 
-            # phi interval
-            h = histos["phiinterval_%s" % layer]
-            h.SetStats(0); h.SetLineWidth(2); h.SetLineColor(col); h.SetFillColor(fcol)
-            if h.GetEntries() == 0:  continue
-            xmin, xmax = find_xinterval(h)
-            h.Rebin(20); h.SetMaximum(h.GetMaximum()*1.3)
+            # intervals
+            for hname, hdesc in [("phiinterval_tt%i_%s", "%s #phi=[%.3f,%.3f]" ),
+                                 ("etainterval_tt%i_%s", "%s #eta=[%.3f,%.3f]" ),
+                                 ("xinterval_tt%i_%s"  , "%s r#phi=[%.3f,%.3f]"),
+                                 ("zinterval_tt%i_%s"  , "%s z=[%.3f,%.3f]"    )]:
+                h = histos[hname % (tt,layer)]
+                h.SetStats(0); h.SetLineWidth(2); h.SetLineColor(col); h.SetFillColor(fcol)
+                if h.GetEntries() == 0:  continue
+                xmin, xmax = find_interval(h)
+                h.Rebin(10); h.SetMaximum(h.GetMaximum()*1.3)
+                h.Draw("hist")
 
-            h.Draw("hist")
-            moveLegend(0.56,0.88,0.94,0.94); legend.Clear()
-            legend.AddEntry(h, "%s #phi=[%.3f,%.3f]" % (layer,xmin,xmax), "f")
-            legend.Draw()
+                moveLegend(0.56,0.88,0.94,0.94); legend.Clear()
+                legend.AddEntry(h, hdesc % (layer,xmin,xmax), "f")
+                legend.Draw()
+                CMS_label()
+                save(imgdir, hname % (tt,layer))
+
+        donotdelete = []
+        return donotdelete
+
+    # Barrel tt27
+    for tt in [27]:
+        histos = bookLayer(tt)
+        projectLayer(tree, histos, tt, nentries=nentries)
+        d = drawLayer(histos, imgdir, tt)
+
+
+# ______________________________________________________________________________
+if sections["multiplicity"]:
+
+    """Before stub cleaning"""
+
+    from ROOT import TFileCollection
+
+    chain2 = TChain("ntupler/tree", "")
+    fc = TFileCollection("fileinfolist", "", infilestxt)
+    chain2.AddFileInfoList(fc.GetList())
+    tree2 = chain2
+
+    latex2 = TLatex()
+    latex2.SetNDC()
+    latex2.SetTextFont(42)
+    latex2.SetTextSize(0.034)
+
+    line2 = TLine()
+    line2.SetLineColor(kGray+3)
+    line2.SetLineStyle(1)
+    line2.SetLineWidth(2)
+
+    in_quantiles = array('d', [0.0015, 0.025, 0.16, 0.5, 0.84, 0.975, 0.9985])
+    quantiles = array('d', [0., 0., 0., 0., 0., 0., 0.])
+
+    phimaxs = [0.006, 0.008, 0.010, 0.014, 0.018, 0.024]
+    #xmaxs = [0.14, 0.30, 0.60, 1.00, 1.60, 2.60]
+    xmaxs = [0.6, 0.6, 0.6, 1.2, 1.2, 1.2]
+    zmaxs = [0.6, 0.6, 0.6, 6, 6, 6]
+    #xcuts = [0.030, 0.070, 0.130, 0.270, 0.540, 0.810]
+    #zcuts = [0.150, 0.300, 0.300, 5.000, 5.000, 5.000]
+    xcuts = [0.040, 0.070, 0.130, 0.240, 0.450, 0.780]
+    zcuts = [0.150, 0.200, 0.300, 3.000, 3.500, 4.000]
+
+    def bookLayer():
+        histos = {}
+        for i in xrange(5,11):
+            layer = "b%i" % i
+            ii = i-5
+
+            h = TH1F("nstubs_%s" % layer, "; # stubs_{%s}; Entries" % layer, 10, 0, 10)
+            histos[h.GetName()] = h
+            h = TH1F("phiresidual_%s" % layer, "; stub #phi_{%s} - ideal #phi_{%s}; Entries" % (layer, layer), 240, -phimaxs[ii], phimaxs[ii])
+            histos[h.GetName()] = h
+            h = TH1F("xresidual_%s" % layer, "; stub r#phi_{%s} - ideal r#phi_{%s} [cm]; Entries" % (layer, layer), 240, -xmaxs[ii], xmaxs[ii])
+            histos[h.GetName()] = h
+            h = TH1F("zresidual_%s" % layer, "; stub z_{%s} - ideal z_{%s} [cm]; Entries" % (layer, layer), 240, -zmaxs[ii], zmaxs[ii])
+            histos[h.GetName()] = h
+
+            h = TH1F("phiresidual_clean_%s" % layer, "; stub #phi_{%s} - ideal #phi_{%s}; Entries" % (layer, layer), 240, -phimaxs[ii], phimaxs[ii])
+            histos[h.GetName()] = h
+            h = TH1F("xresidual_clean_%s" % layer, "; stub r#phi_{%s} - ideal r#phi_{%s} [cm]; Entries" % (layer, layer), 240, -xmaxs[ii], xmaxs[ii])
+            histos[h.GetName()] = h
+            h = TH1F("zresidual_clean_%s" % layer, "; stub z_{%s} - ideal z_{%s} [cm]; Entries" % (layer, layer), 240, -zmaxs[ii], zmaxs[ii])
+            histos[h.GetName()] = h
+        return histos
+
+    def projectLayer(tree, tree2, histos, nentries=1000000000):
+        tree.SetBranchStatus("*", 0)
+        tree.SetBranchStatus("genParts_pt"    , 1)
+        tree.SetBranchStatus("genParts_eta"   , 1)
+        tree.SetBranchStatus("genParts_phi"   , 1)
+        tree.SetBranchStatus("genParts_vz"    , 1)
+        tree.SetBranchStatus("genParts_charge", 1)
+        tree.SetBranchStatus("TTStubs_r"      , 1)
+        tree.SetBranchStatus("TTStubs_phi"    , 1)
+        tree.SetBranchStatus("TTStubs_z"      , 1)
+        tree.SetBranchStatus("TTStubs_modId"  , 1)
+
+        tree2.SetBranchStatus("*", 0)
+        tree2.SetBranchStatus("TTStubs_r"      , 1)
+        tree2.SetBranchStatus("TTStubs_phi"    , 1)
+        tree2.SetBranchStatus("TTStubs_z"      , 1)
+        tree2.SetBranchStatus("TTStubs_modId"  , 1)
+
+        ievt, ievt2 = tree, tree2
+        for i_ievt in xrange(nentries):
+            tree.GetEntry(i_ievt)
+            tree2.GetEntry(i_ievt)
+
+            simPt, simEta, simPhi, simVZ, simCharge = ievt.genParts_pt[0], ievt.genParts_eta[0], ievt.genParts_phi[0], ievt.genParts_vz[0], ievt.genParts_charge[0]
+
+            # Exactly 6 stubs
+            if len(ievt.TTStubs_r) == 6:
+                for r, phi, z, moduleId in izip(ievt.TTStubs_r, ievt.TTStubs_phi, ievt.TTStubs_z, ievt.TTStubs_modId):
+                    lay = decodeLayer(moduleId)
+                    layer = "b%i" % lay
+                    if lay > 10:  print "ERROR! lay:", lay
+
+                    deltaPhi = - 0.3*3.8*r*1e-2 * (simCharge/simPt) / 2.0
+                    idealPhi = simPhi + deltaPhi
+                    idealZ = simVZ + r*sinh(simEta)  # cot(theta) = sinh(eta) = pz/pt
+
+                    histos["phiresidual_clean_%s" % layer].Fill(phi - idealPhi)
+                    histos["xresidual_clean_%s" % layer].Fill(r * (phi - idealPhi))
+                    histos["zresidual_clean_%s" % layer].Fill(z - idealZ)
+
+                assert(len(ievt2.TTStubs_r) >= len(ievt.TTStubs_r))
+                nstubs = [0, 0, 0, 0, 0, 0]
+                for r, phi, z, moduleId in izip(ievt2.TTStubs_r, ievt2.TTStubs_phi, ievt2.TTStubs_z, ievt2.TTStubs_modId):
+                    lay = decodeLayer(moduleId)
+                    layer = "b%i" % lay
+                    if lay > 10:  continue
+                    ii = lay - 5
+
+                    deltaPhi = - 0.3*3.8*r*1e-2 * (simCharge/simPt) / 2.0
+                    idealPhi = simPhi + deltaPhi
+                    idealZ = simVZ + r*sinh(simEta)  # cot(theta) = sinh(eta) = pz/pt
+
+                    histos["phiresidual_%s" % layer].Fill(phi - idealPhi)
+                    histos["xresidual_%s" % layer].Fill(r * (phi - idealPhi))
+                    histos["zresidual_%s" % layer].Fill(z - idealZ)
+                    nstubs[ii] += 1
+
+                for i in xrange(5,11):
+                    layer = "b%i" % i
+                    ii = i-5
+                    histos["nstubs_%s" % layer].Fill(nstubs[ii])
+
+        tree.SetBranchStatus("*", 1)
+        tree2.SetBranchStatus("*", 1)
+        return
+
+    def drawLayer(histos, imgdir):
+
+        for i in xrange(5,11):
+            layer = "b%i" % i
+            ii = i-5
+
+            # nstubs
+            h0 = histos["nstubs_%s" % layer]
+            h0.SetLineWidth(2); h0.SetLineColor(kBlack); h0.SetFillColor(kGray+1)
+            h0.SetMaximum(h0.GetMaximum()*10)
+            h0.Draw("hist")
+            gPad.SetLogy(1)
             CMS_label()
-            save(imgdir, "phiinterval_tt%i_%s" % (tt,layer))
+            save(imgdir, "multiplicity_"+h0.GetName())
+            gPad.SetLogy(0)
 
-            # eta interval
-            h = histos["etainterval_%s" % layer]
-            h.SetStats(0); h.SetLineWidth(2); h.SetLineColor(col); h.SetFillColor(fcol)
-            if h.GetEntries() == 0:  continue
-            xmin, xmax = find_xinterval(h)
-            h.Rebin(20); h.SetMaximum(h.GetMaximum()*1.3)
+            # residuals
+            for hname in ["phiresidual_%s",
+                          "xresidual_%s",
+                          "zresidual_%s"]:
+                h0 = histos[hname % layer]
+                h0.SetLineWidth(2); h0.SetLineColor(kBlack); h0.SetFillColor(kGray+1)
+                ymax = h0.GetMaximum()
+                h0.SetMinimum(0); h0.SetMaximum(ymax*1.4)
+                h0.SetStats(0); h0.Draw("hist")
 
-            h.Draw("hist")
-            moveLegend(0.56,0.88,0.94,0.94); legend.Clear()
-            legend.AddEntry(h, "%s #eta=[%.3f,%.3f]" % (layer,xmin,xmax), "f")
-            legend.Draw()
-            CMS_label()
-            save(imgdir, "etainterval_tt%i_%s" % (tt,layer))
+                h1 = histos[(hname.replace("residual_", "residual_clean_")) % layer]
+                h1.SetLineWidth(2); h1.SetLineColor(col); h1.SetFillColor(fcol)
+                h1.Draw("same hist")
 
-            # z interval
-            h = histos["zinterval_%s" % layer]
-            h.SetStats(0); h.SetLineWidth(2); h.SetLineColor(col); h.SetFillColor(fcol)
-            if h.GetEntries() == 0:  continue
-            xmin, xmax = find_xinterval(h)
-            h.Rebin(20); h.SetMaximum(h.GetMaximum()*1.3)
+                moveLegend(0.56,0.74,0.92,0.94); legend.Clear()
+                legend.AddEntry(h0, "default #splitline{#mu=%.3e}{#sigma=%.3e}" % (h0.GetMean(),h0.GetRMS()), "f")
+                legend.AddEntry(h1, "cleaned #splitline{#mu=%.3e}{#sigma=%.3e}" % (h1.GetMean(),h1.GetRMS()), "f")
+                legend.Draw()
 
-            h.Draw("hist")
-            moveLegend(0.56,0.88,0.94,0.94); legend.Clear()
-            legend.AddEntry(h, "%s z=[%.3f,%.3f]" % (layer,xmin,xmax), "f")
-            legend.Draw()
-            CMS_label()
-            save(imgdir, "zinterval_tt%i_%s" % (tt,layer))
+                if "xresidual" in hname:
+                    line2.DrawLine(xcuts[ii], 0, xcuts[ii], ymax)
+                    line2.DrawLine(-xcuts[ii], 0, -xcuts[ii], ymax)
+
+                    h1.GetQuantiles(len(quantiles), quantiles, in_quantiles)
+                    eff = h1.Integral(h1.FindFixBin(-xcuts[ii]), h1.FindFixBin(xcuts[ii])) / h1.Integral(0,999999) * 100.
+                    latex2.DrawLatex(0.2, 0.88, "cut:  #Delta < %.3f cm" % xcuts[ii])
+                    latex2.DrawLatex(0.2, 0.84, "efficiency: %.1f%%" % eff)
+                    print h0.GetName(), h1.GetMean()+2.0*h1.GetRMS(), quantiles[5], eff
+
+                elif "zresidual" in hname:
+                    line2.DrawLine(zcuts[ii], 0, zcuts[ii], ymax)
+                    line2.DrawLine(-zcuts[ii], 0, -zcuts[ii], ymax)
+
+                    h1.GetQuantiles(len(quantiles), quantiles, in_quantiles)
+                    eff = h1.Integral(h1.FindFixBin(-zcuts[ii]), h1.FindFixBin(zcuts[ii])) / h1.Integral(0,999999) * 100.
+                    latex2.DrawLatex(0.2, 0.88, "cut:  #Delta < %.3f cm" % zcuts[ii])
+                    latex2.DrawLatex(0.2, 0.84, "efficiency: %.1f%%" % eff)
+                    print h0.GetName(), h1.GetMean()+2.0*h1.GetRMS(), quantiles[5], eff
+
+                CMS_label()
+                save(imgdir, "multiplicity_"+h0.GetName())
 
         donotdelete = []
         return donotdelete
 
 
-    for tt in [27]:
-        histos = bookLayer()
-        projectLayer(tree, histos, tt, nentries=nentries)
-        d = drawLayer(histos, imgdir, tt)
+    histos = bookLayer()
+    projectLayer(tree, tree2, histos, nentries=nentries)
+    d = drawLayer(histos, imgdir)
+
