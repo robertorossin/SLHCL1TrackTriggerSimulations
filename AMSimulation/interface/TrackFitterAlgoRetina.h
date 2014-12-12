58e0700f051539a9ce29803e88daa94651c99e54
Massimo Casarsa <massimo.casarsa@ts.infn.it>
2014-12-12 12:05:13 +0100

    Added code for the retina fitter.

---
+++ AMSimulation/interface/TrackFitterAlgoRetina.h
@@ -0,0 +1,49 @@
+#ifndef _TRACKFITTERALGORETINA_H_
+#define _TRACKFITTERALGORETINA_H_
+
+
+//
+// Implementation of a track fitter based on an artificial retina
+//
+
+#include <map>
+
+#include "SLHCL1TrackTriggerSimulations/AMSimulation/interface/TrackFitterAlgoBase.h"
+#include "SLHCL1TrackTriggerSimulations/AMSimulation/interface/Retina.h"
+using namespace slhcl1tt;
+
+
+class TrackFitterAlgoRetina : public TrackFitterAlgoBase {
+
+ private:
+
+  // This contains the retina configuration parameters,
+  // it is filled in initialize():
+  std::map <const char*,double> config[3];
+
+  // This is used to rotate the trigger tower into the first quadrant:
+  const double rot_angle[8] = {  0.39269908169872414,   //  pi/8
+				-0.39269908169872414,   // -pi/8
+				-1.17809724509617242,   // -3/8 pi
+				-1.96349540849362070,   // -5/8 pi
+				-2.74889357189106898,   // -7/8 pi
+				-3.53429173528851726,   // -9/8 pi
+				-4.31968989868596509,   // -11/8 pi
+				-5.10508806208341426 }; // -13/8 pi
+
+  void initialize();
+  void rotateHits(std::vector<TTHit>& hits, double angle);
+  void confTrans(std::vector<TTHit>& hits);
+
+
+ public:
+
+  TrackFitterAlgoRetina();
+
+  ~TrackFitterAlgoRetina();
+
+  int fit(std::vector<TTHit>& hits, std::vector<TTTrack2>& tracks);
+
+};
+
+#endif
--- AMSimulation/src/TrackFitter.cc
+++ AMSimulation/src/TrackFitter.cc
@@ -47,13 +47,105 @@ int TrackFitter::makeTracks(TString src, TString out) {
         }
 
         // _____________________________________________________________________
-        // Arrange track fit combinations
+        // Retina fit
 
         std::vector<TTTrack2> tracks;
         tracks.reserve(200);
 
-        // Loop over the roads
-        for (unsigned i=0, j=0; i<nroads; ++i) {
+	if (po.mode == 2) {
+
+	  // Loop over the roads
+	  for (unsigned iroad=0; iroad<nroads; ++iroad) {
+
+            const unsigned nstubs = reader.vr_stubRefs->at(iroad).size();
+	    const int triggerTower = 25;    /////// =============> trigger tower set by hand !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	    //std::cout << reader.vr_triggerTowerId->at(iroad) << std::endl;
+
+            if (verbose_>2)  std::cout << Debug() << "... ... road: " << iroad 
+				       << " # stubs: " << nstubs <<  std::endl;
+
+            std::vector<TTHit> hits;
+	    
+            for (unsigned istub=0; istub<nstubs; ++istub) {
+	      unsigned ref = reader.vr_stubRefs->at(iroad).at(istub);
+
+	      hits.emplace_back( TTHit{                // using POD type constructor
+		    ref,
+		    reader.vb_x->at(ref),
+		    reader.vb_y->at(ref),
+		    reader.vb_z->at(ref),
+		    (float) sqrt(reader.vb_x->at(ref)*reader.vb_x->at(ref) +
+		                 reader.vb_y->at(ref)*reader.vb_y->at(ref) ),
+		    reader.vb_r->at(ref),
+                    reader.vb_phi->at(ref),
+                    0.,
+                    0.,
+                    0.
+		    });
+            
+	    } // loop over stubs
+
+            // Fit
+	    fitterRetina_->setEvent(ievt);
+	    fitterRetina_->setTriggerTower(triggerTower);
+	    int status = fitterRetina_->fit(hits, tracks);
+
+            if (verbose_>2)  std::cout << Debug() << "... ... # tracks: " << tracks.size() 
+				       << " status: " << status << " ... " << std::endl;
+
+	    
+	    //////////////////////////////////////////////////////////////
+	    // This printout is just for debug purposes:
+
+	    if (tracks.size()>0) {
+	      std::cout << " ----------------------------------------------------------------" << std::endl;
+	      std::cout << "  Generated particles:" << std::endl;
+
+	      for (unsigned ipart=0; ipart<reader.vp_pt->size(); ++ipart) {
+
+		// curvature and helix radius:
+		double R = reader.vp_pt->at(ipart)/(0.003*3.8);
+
+		// helix center:
+		double x0 = reader.vp_vx->at(ipart) - reader.vp_charge->at(ipart)*R*std::sin(reader.vp_phi->at(ipart));
+		double y0 = reader.vp_vy->at(ipart) + reader.vp_charge->at(ipart)*R*std::cos(reader.vp_phi->at(ipart));
+
+		// transverse and longitudinal impact parameters:
+		double d0 = reader.vp_charge->at(ipart)*(std::sqrt(x0*x0+y0*y0)-R);
+		double theta = 2.*std::atan(std::exp(-reader.vp_eta->at(ipart)));
+		double z0 = reader.vp_vz->at(ipart) - 2.*R/std::tan(theta)* 
+		  asin(0.5/R*std::sqrt((reader.vp_vx->at(ipart)*reader.vp_vx->at(ipart)+
+					reader.vp_vy->at(ipart)*reader.vp_vy->at(ipart)-d0*d0)/(1.+d0/R)));
+
+		std::cout << "  " << ipart << "  -  " << reader.vp_pt->at(ipart) << " " << reader.vp_phi->at(ipart) << " " 
+			  << reader.vp_eta->at(ipart) << " " << z0 << std::endl;
+	      }
+
+	      std::cout << " Fitted tracks:" << std::endl;
+	      for (unsigned itrk=0; itrk<tracks.size(); ++itrk) {
+		TTTrack2 track = tracks.at(itrk);
+		const std::vector<unsigned>& stubRefs = track.stubRefs();
+		
+		std::cout << "  " << itrk << "  -  " << track.pt() << " " << track.phi0() << " " 
+			  << track.eta() << " " << track.z0() << std::endl;
+		//std::cout << stubRefs.size() << std::endl;
+
+	      }
+	    }
+
+	    ////////////////////////////////////////////////////////////////
+
+	  } // loop over roads
+
+	} 
+
+        // _____________________________________________________________________
+        // Arrange track fit combinations
+
+	else {
+
+	  // Loop over the roads
+	  for (unsigned i=0, j=0; i<nroads; ++i) {
             // # superstrips, # stubs
             const unsigned nsuperstrips = reader.vr_nsuperstrips->at(i);
             const unsigned nstubs = reader.vr_stubRefs->at(i).size();
@@ -66,92 +158,100 @@ int TrackFitter::makeTracks(TString src, TString out) {
             id_type ssId = 0, old_ssId = 0;
 
             for (j=0; j<nstubs; ++j) {
-                ref = reader.vr_stubRefs->at(i).at(j);
-                ssId = reader.vr_stubSuperstripIds->at(i).at(j);
-                if (verbose_>2)  std::cout << Debug() << "... ... ... stub: " << j << " ssId: " << ssId << " ref: " << ref << std::endl;
+	      ref = reader.vr_stubRefs->at(i).at(j);
+	      ssId = reader.vr_stubSuperstripIds->at(i).at(j);
+	      if (verbose_>2)  std::cout << Debug() << "... ... ... stub: " << j << " ssId: " << ssId << " ref: " << ref << std::endl;
 
-                if (j == 0 || ssId != old_ssId) {
-                    stubRefsGrouped.push_back(std::vector<unsigned>());
-                }
-                stubRefsGrouped.back().push_back(ref);
-                old_ssId = ssId;
+	      if (j == 0 || ssId != old_ssId) {
+		stubRefsGrouped.push_back(std::vector<unsigned>());
+	      }
+	      stubRefsGrouped.back().push_back(ref);
+	      old_ssId = ssId;
             }
 
             if (verbose_>3) {
-                for (j=0; j<stubRefsGrouped.size(); ++j)
-                    std::cout << Debug() << "... ... ... superstrip: " << j << " # stubRefs: " << stubRefsGrouped.at(j).size() << std::endl;
+	      for (j=0; j<stubRefsGrouped.size(); ++j)
+		std::cout << Debug() << "... ... ... superstrip: " << j << " # stubRefs: " << stubRefsGrouped.at(j).size() << std::endl;
             }
 
             // Make combinations
             if (nsuperstrips == stubRefsGrouped.size()) {
-                std::vector<unsigned> stubRefs;
-                std::vector<unsigned> indices(nsuperstrips, 0);  // init to zeroes
-                int ii, jj;
+	      std::vector<unsigned> stubRefs;
+	      std::vector<unsigned> indices(nsuperstrips, 0);  // init to zeroes
+	      int ii, jj;
 
-                j = 0;
-                while (true) {
-                    stubRefs.clear();
-                    for (ii=0; ii<int(nsuperstrips); ++ii)
-                        stubRefs.push_back(stubRefsGrouped.at(ii).at(indices[ii]));
-                    tracks.emplace_back(i, j, stubRefs);
-                    if ((int) tracks.size() >= maxTracks_)
-                        break;
+	      j = 0;
+	      while (true) {
+		stubRefs.clear();
+		for (ii=0; ii<int(nsuperstrips); ++ii)
+		  stubRefs.push_back(stubRefsGrouped.at(ii).at(indices[ii]));
+		tracks.emplace_back(i, j, stubRefs);
+		if ((int) tracks.size() >= maxTracks_)
+		  break;
 
-                    for (ii=nsuperstrips-1; ii>=0; --ii)
-                        if (indices[ii] != stubRefsGrouped.at(ii).size() - 1)
-                            break;  // take the last index that has not reached the end
-                    if (ii == -1)  break;
+		for (ii=nsuperstrips-1; ii>=0; --ii)
+		  if (indices[ii] != stubRefsGrouped.at(ii).size() - 1)
+		    break;  // take the last index that has not reached the end
+		if (ii == -1)  break;
 
-                    indices[ii] += 1;  // increment that index
-                    for (jj = ii+1; jj<int(nsuperstrips); ++jj)
-                        indices[jj] = 0;  // set indices behind that index to zeroes
+		indices[ii] += 1;  // increment that index
+		for (jj = ii+1; jj<int(nsuperstrips); ++jj)
+		  indices[jj] = 0;  // set indices behind that index to zeroes
 
-                    ++j;
-                }
+		++j;
+	      }
 
             } else {
-                std::cout << Warning() << "Disagreement between nsuperstrips and stubRefsGrouped.size(): " << nsuperstrips << " vs " << stubRefsGrouped.size() << std::endl;
+	      std::cout << Warning() << "Disagreement between nsuperstrips and stubRefsGrouped.size(): " << nsuperstrips << " vs " << stubRefsGrouped.size() << std::endl;
             }
-        }
+	  }
 
-        // _____________________________________________________________________
-        // Call track fitter on track candidates
+	  // _____________________________________________________________________
+	  // Call track fitter on track candidates
 
-        const unsigned ntracks = tracks.size();
-        int status = 0;
+	  const unsigned ntracks = tracks.size();
+	  int status = 0;
 
-        // Loop over the track candidates
-        for (unsigned i=0, j=0; i<ntracks; ++i) {
+	  // Loop over the track candidates
+	  for (unsigned i=0, j=0; i<ntracks; ++i) {
             TTTrack2& track = tracks.at(i);
             const std::vector<unsigned>& stubRefs = track.stubRefs();
 
             if (verbose_>2) {
-                std::cout << Debug() << "... ... track: " << i << " roadRef: " << track.roadRef() << " combRef: " << track.combRef() << " stubRefs: ";
-                std::copy(stubRefs.begin(), stubRefs.end(), std::ostream_iterator<unsigned>(std::cout, " "));
-                std::cout << std::endl;
+	      std::cout << Debug() << "... ... track: " << i << " roadRef: " << track.roadRef() << " combRef: " << track.combRef() << " stubRefs: ";
+	      std::copy(stubRefs.begin(), stubRefs.end(), std::ostream_iterator<unsigned>(std::cout, " "));
+	      std::cout << std::endl;
             }
 
             std::vector<TTHit> hits;
             for (j=0; j<stubRefs.size(); ++j) {
-                const unsigned& ref = stubRefs.at(j);
-                hits.emplace_back(TTHit{                // using POD type constructor
-                    reader.vb_r->at(ref),
+	      const unsigned& ref = stubRefs.at(j);
+	      hits.emplace_back(TTHit{                // using POD type constructor
+		    ref,
+		    reader.vb_x->at(ref),
+		    reader.vb_y->at(ref),
+		    reader.vb_z->at(ref),
+		    (float) sqrt(reader.vb_x->at(ref)*reader.vb_x->at(ref) +
+		                 reader.vb_y->at(ref)*reader.vb_y->at(ref) ),
+		    reader.vb_r->at(ref),
                     reader.vb_phi->at(ref),
-                    reader.vb_z->at(ref),
                     0.,
                     0.,
                     0.
-                });
+		    });
             }
 
             // Fit
             if (po.mode == 0)
-                status = fitterLin_->fit(hits, track);
-            else
-                status = fitterDas_->fit(hits, track);
+	      status = fitterLin_->fit(hits, track);
+	    else
+	      status = fitterDas_->fit(hits, track);
 
             if (verbose_>2)  std::cout << Debug() << "... ... track: " << i << " status: " << status << " ... " << std::endl;
-        }
+	  }
+
+	} // if po.mode
+
 
         // _____________________________________________________________________
         // Remove fails and duplicates
