c79c6a81b5465d9f65ac90e9ccb4fd6ec7fcddcb
Jia Fu Low <jia.fu.low@cern.ch>
2015-01-09 17:41:22 -0600

    Updated drawer scripts

--- AMSimulation/res/drawer_patternbank.py
+++ AMSimulation/res/drawer_patternbank.py
@@ -3,13 +3,13 @@
 from rootdrawing import *
 from roothelper import *
 from array import array
+import glob
 
 # ______________________________________________________________________________
 # Configurations
 
 sections = {}
-sections["fixed"     ] = True
-sections["projective"] = False
+sections["coverage"  ] = True
 
 drawerInit = DrawerInit()
 gStyle.SetPadRightMargin(0.1)
@@ -27,23 +27,28 @@ if gSystem.AccessPathName(imgdir):
 
 
 # ______________________________________________________________________________
-# Fixed
+# Coverage
 
-if sections["fixed"]:
+if sections["coverage"]:
 
     gStyle.SetTitleSize(0.05, "Y")
     latex.SetTextSize(0.05)
 
-    in_superstrips = ["ss32", "ss64", "ss128", "ss256", "ss512", "ss1024"]
-    #in_superstrips = ["ss256"]
-
-    def bookCoverage(bank):
+    def bookCoverage(in_superstrips, bank):
         superstrips = []
         graphs = {}
 
         for ss in in_superstrips:
             chain.Reset(); chain2.Reset()
             infile = (EOS + "/" + bank) % ss
+
+            # Replace the '400M' by wildcard
+            infile = infile.replace('400M', '*')
+            m = glob.glob(infile)
+            if m:
+                infile = m[0]
+            else:
+                print "ERROR: %s not found" % infile
 
             # Pattern bank statistics
             chain2.Add(infile)
@@ -98,7 +103,7 @@ if sections["fixed"]:
         return (superstrips, graphs)
 
     def drawCoverage(superstrips, graphs, xmin=0, xmax=1e8, tower="tt27"):
-        hframe = TH1F("hframe", "; # of patterns; running estimate for coverage", 100, xmin, xmax)
+        hframe = TH1F("hframe", "; # of patterns; running estimate for coverage", 1000, xmin, xmax)
         hframe.SetStats(0); hframe.SetMinimum(0); hframe.SetMaximum(1.2)
         hframe.SetNdivisions(510, "Y")
 
@@ -109,7 +114,7 @@ if sections["fixed"]:
             gr.SetLineColor(paletteSet1[i])
 
         # Legend
-        moveLegend(0.66,0.15,0.96,0.45); legend.Clear()
+        moveLegend(0.66,0.15,0.96,0.15+len(superstrips)*0.05); legend.Clear()
         for i, ss in enumerate(superstrips):
             gr = graphs["gr_%s" % ss[0]]
             legend.AddEntry(gr, ss[0], "l")
@@ -123,7 +128,7 @@ if sections["fixed"]:
             gr.Draw("C")
         legend.Draw()
         CMS_label()
-        save(imgdir, "fixed_sorted_%s" % tower, dot_root=True)
+        save(imgdir, "coverage_sorted_%s_%s" % (superstrips[0][0],tower), dot_root=True)
 
         # Zoom in
         hframe.Draw()
@@ -135,7 +140,7 @@ if sections["fixed"]:
             gr.Draw("C")
         legend.Draw()
         CMS_label()
-        save(imgdir, "fixed_sorted_zoom_%s" % tower)
+        save(imgdir, "coverage_sorted_zoom_%s_%s" % (superstrips[0][0],tower))
 
         # Print out
         for i, ss in enumerate(superstrips):
@@ -148,7 +153,7 @@ if sections["fixed"]:
         return donotdelete
 
     def drawFrequency(superstrips, graphs, xmin=0, xmax=1e8, tower="tt27"):
-        hframe = TH1F("hframe", "; # of patterns; min frequency", 100, xmin, xmax)
+        hframe = TH1F("hframe", "; # of patterns; min frequency", 1000, xmin, xmax)
         hframe.SetStats(0); hframe.SetMinimum(0.5); hframe.SetMaximum(1e5)
         hframe.SetNdivisions(510, "Y")
 
@@ -159,7 +164,7 @@ if sections["fixed"]:
             gr.SetLineColor(paletteSet1[i])
 
         # Legend
-        moveLegend(0.66,0.15,0.96,0.45); legend.Clear()
+        moveLegend(0.66,0.15,0.96,0.15+len(superstrips)*0.05); legend.Clear()
         for i, ss in enumerate(superstrips):
             gr = graphs["gr_freq_%s" % ss[0]]
             legend.AddEntry(gr, ss[0], "l")
@@ -172,7 +177,7 @@ if sections["fixed"]:
             gr.Draw("C")
         legend.Draw()
         CMS_label()
-        save(imgdir, "fixed_freq_%s" % tower, dot_root=True)
+        save(imgdir, "frequency_%s_%s" % (superstrips[0][0],tower), dot_root=True)
 
         # Zoom in
         hframe.Draw()
@@ -182,208 +187,74 @@ if sections["fixed"]:
             gr.Draw("C")
         legend.Draw()
         CMS_label()
-        save(imgdir, "fixed_freq_zoom_%s" % tower)
+        save(imgdir, "frequency_zoom_%s_%s" % (superstrips[0][0],tower))
 
         gPad.SetLogy(0)
         donotdelete = [hframe]
         return donotdelete
 
-    # Barrel 2 GeV
-    bank = "patternBank_sp16_%s_tt27_400M.root"
-    (superstrips, graphs) = bookCoverage(bank)
-    d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27")
-    d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27")
+    if True:  # fixed
+        #in_superstrips = ["ss32", "ss64", "ss128", "ss256", "ss512", "ss1024"]
+        in_superstrips = ["ss64", "ss128", "ss256", "ss512", "ss1024"]
 
-    # Barrel 3 GeV
-    #bank = "patternBank_sp16_%s_tt27_pt3_400M.root"
-    #(superstrips, graphs) = bookCoverage(bank)
-    #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
-    #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
+        # Barrel 2 GeV
+        #bank = "patternBank_sp16_%s_tt27_400M.root"
+        #(superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27")
+        #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27")
 
-    # Barrel 10 GeV
-    #bank = "patternBank_sp16_%s_tt27_pt10_400M.root"
-    #(superstrips, graphs) = bookCoverage(bank)
-    #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
-    #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
+        # Barrel 3 GeV
+        bank = "patternBank_sp16_%s_tt27_pt3_400M.root"
+        (superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
+        d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
 
+        # Barrel 10 GeV
+        #bank = "patternBank_sp16_%s_tt27_pt10_400M.root"
+        #(superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
+        #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
 
-# ______________________________________________________________________________
-# Projective
+    if True:  # projective
+        #in_superstrips = ["600x0", "400x0", "200x0", "200x1", "100x2", "20x10"]
+        in_superstrips = ["600x0", "400x0", "200x0"]
 
-if sections["projective"]:
+        # Barrel 2 GeV
+        #bank = "patternBank_lu%s_tt27_400M.root"
+        #(superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27")
+        #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27")
 
-    gStyle.SetTitleSize(0.05, "Y")
-    latex.SetTextSize(0.05)
+        # Barrel 3 GeV
+        bank = "patternBank_lu%s_tt27_pt3_400M.root"
+        (superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
+        d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
 
-    in_superstrips = ["600x0", "400x0", "200x0", "200x1", "100x2", "20x10"]
-    #in_superstrips = ["400x0"]
+        # Barrel 10 GeV
+        #bank = "patternBank_lu%s_tt27_pt10_400M.root"
+        #(superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
+        #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
 
-    def bookCoverage(bank):
-        superstrips = []
-        graphs = {}
+    if True:  # rational
+        in_superstrips = ["sf0p5", "sf0p7", "sf1", "sf2"]
 
-        for ss in in_superstrips:
-            chain.Reset(); chain2.Reset()
-            infile = (EOS + "/" + bank) % ss
+        # Barrel 2 GeV
+        #bank = "patternBank_%s_nz1_tt27_400M.root"
+        #(superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27")
+        #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27")
 
-            # Pattern bank statistics
-            chain2.Add(infile)
-            assert(chain2.GetEntries() > 0)
-            chain2.GetEntry(0)
-            coverage = chain2.coverage
-            count = chain2.count
+        # Barrel 3 GeV
+        bank = "patternBank_%s_nz1_tt27_pt3_400M.root"
+        (superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
+        d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
 
-            # Pattern bank
-            chain.Add(infile)
-            assert(chain.GetEntries() > 0)
-            chain.SetBranchStatus("superstripIds", 0)
-            npatterns = chain.GetEntriesFast()
-            npoints = 200
-            every = npatterns / npoints
-
-            xvalues, yvalues, zvalues = [], [], []
-            x, x_0p9, integral = 0, 0, 0
-            for i in xrange(npatterns):
-                chain.GetEntry(i)
-                frequency = chain.frequency
-                if i == x:
-                    y = float(integral) / float(count) * coverage
-                    xvalues.append(i); yvalues.append(y); zvalues.append(frequency)
-                    print "..", i, y, frequency
-                    if y < 0.9:  x_0p9 = x
-                    x += every
-
-                elif i == npatterns - 1:
-                    y = float(integral) / float(count) * coverage
-                    xvalues.append(i); yvalues.append(y); zvalues.append(frequency)
-                    print "..", i, y, frequency
-                    if ((integral + frequency) != count):
-                        print "ERROR: frequency overflow: ", integral + frequency, "!=", count
-                        print "       apply temporary fix."
-                        yvalues = [yy * float(count) / float(integral + frequency) for yy in yvalues]
-                    #assert((integral + frequency) == count)
-
-                integral += frequency
-
-            superstrips.append((ss, npatterns, coverage, x_0p9))
-            print superstrips[-1]
-            npoints = len(xvalues)
-            gname = "gr_%s" % ss
-            gr = TGraph(npoints, array('d', xvalues), array('d', yvalues))
-            gr.SetName(gname)
-            graphs[gname] = gr
-            gname = "gr_freq_%s" % ss
-            gr = TGraph(npoints, array('d', xvalues), array('d', zvalues))
-            gr.SetName(gname)
-            graphs[gname] = gr
-        return (superstrips, graphs)
-
-    def drawCoverage(superstrips, graphs, xmin=0, xmax=1e8, tower="tt27"):
-        hframe = TH1F("hframe", "; # of patterns; running estimate for coverage", 100, xmin, xmax)
-        hframe.SetStats(0); hframe.SetMinimum(0); hframe.SetMaximum(1.2)
-        hframe.SetNdivisions(510, "Y")
-
-        # Style
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_%s" % ss[0]]
-            gr.SetLineWidth(2); gr.SetLineStyle(1); gr.SetMarkerSize(0)
-            gr.SetLineColor(paletteSet1[i])
-
-        # Legend
-        moveLegend(0.66,0.15,0.96,0.45); legend.Clear()
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_%s" % ss[0]]
-            legend.AddEntry(gr, ss[0], "l")
-
-        # Draw
-        hframe.Draw()
-        for y in [0.5, 0.8, 0.9, 0.95, 1.0]:
-            line.DrawLine(xmin, y, xmax, y)
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_%s" % ss[0]]
-            gr.Draw("C")
-        legend.Draw()
-        CMS_label()
-        save(imgdir, "projective_sorted_%s" % tower, dot_root=True)
-
-        # Zoom in
-        hframe.Draw()
-        hframe.GetXaxis().SetRangeUser(0, xmax/50)
-        for y in [0.5, 0.8, 0.9, 0.95, 1.0]:
-            line.DrawLine(xmin, y, xmax/50, y)
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_%s" % ss[0]]
-            gr.Draw("C")
-        legend.Draw()
-        CMS_label()
-        save(imgdir, "projective_sorted_zoom_%s" % tower)
-
-        # Print out
-        for i, ss in enumerate(superstrips):
-            print '{0:7} {1:10d}  {2:5.4f}'.format(ss[0], ss[1], ss[2])
-        print
-        for i, ss in enumerate(superstrips):
-            print '{0:7} {1:10d}  {2:5.4f}'.format(ss[0], ss[3], 0.9)
-
-        donotdelete = [hframe]
-        return donotdelete
-
-    def drawFrequency(superstrips, graphs, xmin=0, xmax=1e8, tower="tt27"):
-        hframe = TH1F("hframe", "; # of patterns; min frequency", 100, xmin, xmax)
-        hframe.SetStats(0); hframe.SetMinimum(0.5); hframe.SetMaximum(1e5)
-        hframe.SetNdivisions(510, "Y")
-
-        # Style
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_freq_%s" % ss[0]]
-            gr.SetLineWidth(2); gr.SetLineStyle(1); gr.SetMarkerSize(0)
-            gr.SetLineColor(paletteSet1[i])
-
-        # Legend
-        moveLegend(0.66,0.15,0.96,0.45); legend.Clear()
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_freq_%s" % ss[0]]
-            legend.AddEntry(gr, ss[0], "l")
-
-        # Draw
-        hframe.Draw()
-        gPad.SetLogy(1)
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_freq_%s" % ss[0]]
-            gr.Draw("C")
-        legend.Draw()
-        CMS_label()
-        save(imgdir, "projective_freq_%s" % tower, dot_root=True)
-
-        # Zoom in
-        hframe.Draw()
-        hframe.GetXaxis().SetRangeUser(0, xmax/50)
-        for i, ss in enumerate(superstrips):
-            gr = graphs["gr_freq_%s" % ss[0]]
-            gr.Draw("C")
-        legend.Draw()
-        CMS_label()
-        save(imgdir, "projective_freq_zoom_%s" % tower)
-
-        gPad.SetLogy(0)
-        donotdelete = [hframe]
-        return donotdelete
-
-    # Barrel 2 GeV
-    bank = "patternBank_lu%s_tt27_400M.root"
-    (superstrips, graphs) = bookCoverage(bank)
-    d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27")
-    d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27")
-
-    # Barrel 3 GeV
-    #bank = "patternBank_lu%s_tt27_pt3_400M.root"
-    #(superstrips, graphs) = bookCoverage(bank)
-    #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
-    #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt3")
-
-    # Barrel 10 GeV
-    #bank = "patternBank_lu%s_tt27_pt10_400M.root"
-    #(superstrips, graphs) = bookCoverage(bank)
-    #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
-    #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
+        # Barrel 10 GeV
+        #bank = "patternBank_%s_nz1_tt27_pt10_400M.root"
+        #(superstrips, graphs) = bookCoverage(in_superstrips, bank)
+        #d = drawCoverage(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
+        #d = drawFrequency(superstrips, graphs, xmax=5e7, tower="tt27_pt10")
 
