0deab264a10a378edc167d403afde650b1e8359b
Jia Fu Low <jia.fu.low@cern.ch>
2015-03-07 12:16:24 -0600

    Improved Picky class
    Radius ranking has changed. Suppose r1 < r2 < r3 < r4 are the four
    radii, and <r> is the mean radius. The preference is r2 > r3 > r1 > r4
    because r2 and r3 are closer to <r> than r1 and r4.
    
    No cuts on phi residual by default

--- AMSimulation/src/Picky.cc
+++ AMSimulation/src/Picky.cc
@@ -1,5 +1,4 @@
 #include "SLHCL1TrackTriggerSimulations/AMSimulation/interface/Picky.h"
-#include "SLHCL1TrackTriggerSimulations/AMSimulationDataFormats/interface/Helper.h"
 using namespace slhcl1tt;
 
 #include <algorithm>
@@ -8,13 +7,12 @@ using namespace slhcl1tt;
 #include <iostream>
 #include <stdexcept>
 
+static const unsigned NLAYERS = 6;
+static const unsigned NDISKS = 5;
 
-Picky::Picky()
- :  simPt_    (-9999.),
-    simEta_   (-9999.),
-    simPhi_   (-9999.),
-    simVZ_    (-9999.),
-    simCharge_(-999999) {
+
+// _____________________________________________________________________________
+Picky::Picky() {
 
     // 4 possible r coordinates per barrel layer
     barrel_r_coords_ = {
@@ -26,20 +24,22 @@ Picky::Picky()
         106.4078,107.0122,108.4078,109.0122
     };
 
-    // 6 possible z coordinates per endcap disk
+    // 4 possible z coordinates per endcap disk
     endcap_z_coords_ = {
-        128.494,130.074,130.294,132.614,134.194,134.414,
-        153.295,154.875,155.095,157.415,158.995,159.215,
-        182.654,184.234,184.454,186.774,188.354,188.574,
-        217.408,218.988,219.208,221.528,223.108,223.328,
-        258.550,260.130,260.350,262.670,264.250,264.470
+        128.494,130.074,132.614,134.194,
+        153.295,154.875,157.415,158.995,
+        182.654,184.234,186.774,188.354,
+        217.408,218.988,221.528,223.108,
+        258.550,260.130,262.670,264.250
     };
 
     // phi cut per barrel layer
-    barrel_phi_cuts_ = { 0.004, 0.004, 0.005, 0.007, 0.009, 0.012 };
+    //barrel_phi_cuts_ = { 0.004, 0.004, 0.005, 0.007, 0.009, 0.012 };
+    barrel_phi_cuts_ = { 0.100, 0.100, 0.100, 0.100, 0.100, 0.100 };  // disabled
 
     // z cut per barrel layer
-    barrel_z_cuts_   = { 0.300, 0.400, 0.500, 3.000, 3.500, 4.000 };
+    //barrel_z_cuts_   = { 0.300, 0.400, 0.500, 3.000, 3.500, 4.000 };
+    barrel_z_cuts_   = { 9.900, 9.900, 9.900, 9.900, 9.900, 9.900 };  // disabled
 
     // phi cut per endcap disk
     endcap_phi_cuts_ = { 0.100, 0.100, 0.100, 0.100, 0.100 };  // disabled
@@ -47,56 +47,59 @@ Picky::Picky()
     // r cut per endcap disk
     endcap_r_cuts_   = { 9.900, 9.900, 9.900, 9.900, 9.900 };  // disabled
 
-    assert(barrel_r_coords_.size() == 6 * 4 && barrel_phi_cuts_.size() == 6 && barrel_z_cuts_.size() == 6);
-    assert(endcap_z_coords_.size() == 5 * 6 && endcap_phi_cuts_.size() == 5 && endcap_r_cuts_.size() == 5);
+    assert(barrel_r_coords_.size() == NLAYERS * 4 && barrel_phi_cuts_.size() == NLAYERS && barrel_z_cuts_.size() == NLAYERS);
+    assert(endcap_z_coords_.size() == NDISKS * 4 && endcap_phi_cuts_.size() == NDISKS && endcap_r_cuts_.size() == NDISKS);
 }
 
-
-void Picky::setTrack(float pt, float eta, float phi, float vz, int charge) {
-    simPt_     = pt;
-    simEta_    = eta;
-    simPhi_    = phi;
-    simVZ_     = vz;
-    simCharge_ = charge;
-}
-
-bool Picky::applyCuts(unsigned lay16, const float& absdphi, const float& absdz, const float& absdr) {
+// _____________________________________________________________________________
+bool Picky::applyCuts(unsigned lay16, const float dphi, const float dr, const float dz) {
     if (lay16 < 6) {
-        return (absdphi < barrel_phi_cuts_.at(lay16) && absdz < barrel_z_cuts_.at(lay16));
+        return (std::abs(dphi) < barrel_phi_cuts_.at(lay16) && std::abs(dz) < barrel_z_cuts_.at(lay16));
 
     } else if (lay16 < 11) {
-        return (absdphi < endcap_phi_cuts_.at(lay16-6) && absdr < endcap_r_cuts_.at(lay16-6));
+        lay16 -= 6;
+        return (std::abs(dphi) < endcap_phi_cuts_.at(lay16) && std::abs(dr) < endcap_r_cuts_.at(lay16));
 
     } else if (lay16 < 16) {
-        return (absdphi < endcap_phi_cuts_.at(lay16-11) && absdr < endcap_r_cuts_.at(lay16-11));
+        lay16 -= 11;
+        return (std::abs(dphi) < endcap_phi_cuts_.at(lay16) && std::abs(dr) < endcap_r_cuts_.at(lay16));
 
     }
     return true;
 }
 
-unsigned Picky::findInterior(unsigned lay16, const float& z, const float& r) {
+// _____________________________________________________________________________
+unsigned Picky::findRank(unsigned lay16, const float r, const float z) {
+    static const float eps = 5e-3;
+
+    unsigned j=3;
     if (lay16 < 6) {
-        for (unsigned i=lay16*4, j=0; i<barrel_r_coords_.size()-1; ++i, ++j)
-            if (r < barrel_r_coords_.at(i+1))
-                return j;
-        return 4-1;
+        for (unsigned i=0; i<4; ++i, --j)
+            if (r > (barrel_r_coords_.at(lay16*4 + j) - eps))
+                break;
 
     } else if (lay16 < 11) {
-        for (unsigned i=(lay16-6)*4, j=0; i<endcap_z_coords_.size()-1; ++i, ++j)
-            if (z < endcap_z_coords_.at(i+1))
-                return j;
-        return 6-1;
+        lay16 -= 6;
+        for (unsigned i=0; i<4; ++i, --j)
+            if (std::abs(z) > (endcap_z_coords_.at(lay16*4 + j) - eps))
+                break;
 
     } else if (lay16 < 16) {
-        for (unsigned i=(lay16-11)*4, j=0; i<endcap_z_coords_.size()-1; ++i, ++j)
-            if (z < endcap_z_coords_.at(i+1))
-                return j;
-        return 6-1;
-
+        lay16 -= 11;
+        for (unsigned i=0; i<4; ++i, --j)
+            if (std::abs(z) > (endcap_z_coords_.at(lay16*4 + j) - eps))
+                break;
     }
-    return 99;
+    assert(j <= 3);
+
+    // j=3 has larger radius than j=2,1,0
+    // j=1,2 are closer to average radius than j=3,4
+    // For future: in endcap, stubs in PS module should rank higher than stubs in 2S module
+    static const float ranks[4] = {2,0,1,3};
+    return ranks[j];
 }
 
+// _____________________________________________________________________________
 void Picky::print() {
     std::cout << "barrel r: ";
     std::copy(barrel_r_coords_.begin(), barrel_r_coords_.end(), std::ostream_iterator<float>(std::cout, " "));
