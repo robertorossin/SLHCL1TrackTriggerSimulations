92e579532e718df37df19b71a6835682a9710675
Jia Fu Low <jia.fu.low@cern.ch>
2015-03-07 12:00:49 -0600

    Cleaned up AMSimulationDataFormats/src/classes.h, AMSimulationDataFormats/src/classes_def.xml

--- AMSimulationDataFormats/src/classes.h
+++ AMSimulationDataFormats/src/classes.h
@@ -1,21 +1,10 @@
 #include <vector>
-////#include "DataFormats/Common/interface/Wrapper.h"
-//#include "SLHCL1TrackTriggerSimulations/AMSimulationDataFormats/interface/TTSuperstrip.h"
-//#include "SLHCL1TrackTriggerSimulations/AMSimulationDataFormats/interface/TTPattern.h"
+//#include "SLHCL1TrackTriggerSimulations/AMSimulationDataFormats/interface/TTRoad.h"
+//#include "SLHCL1TrackTriggerSimulations/AMSimulationDataFormats/interface/TTHit.h"
 
 namespace {
   struct dictionary {
-    ////edm::Wrapper<slhcl1tt::TTSuperstrip> pippo1;
-    ////edm::Wrapper<slhcl1tt::TTHit> pippo2;
-    ////edm::Wrapper<slhcl1tt::TTPattern> pippo3;
-    ////edm::Wrapper<slhcl1tt::TTRoad> pippo4;
-    //std::vector<slhcl1tt::TTSuperstrip> vpippo1;
+    //std::vector<slhcl1tt::TTRoad> vpippo1;
     //std::vector<slhcl1tt::TTHit> vpippo2;
-    //std::vector<slhcl1tt::TTPattern> vpippo3;
-    //std::vector<slhcl1tt::TTRoad> vpippo4;
-    ////edm::Wrapper<std::vector<slhcl1tt::TTSuperstrip> > wvpippo1;
-    ////edm::Wrapper<std::vector<slhcl1tt::TTHit> > wvpippo2;
-    ////edm::Wrapper<std::vector<slhcl1tt::TTPattern> > wvpippo3;
-    ////edm::Wrapper<std::vector<slhcl1tt::TTRoad> > wvpippo4;
   };
 }
