cd56e5a6d1becf90eb83207fc239bf527e8778a3
Jia Fu Low <jia.fu.low@cern.ch>
2015-03-13 14:19:39 -0500

    Changed min 1/pT threshold from 0.005 to 0.0005

--- Configuration/python/tower/SingleMuonFlatOneOverPt0p0005To0p5_tt27_cfi.py
+++ Configuration/python/tower/SingleMuonFlatOneOverPt0p0005To0p5_tt27_cfi.py
@@ -3,21 +3,21 @@ import FWCore.ParameterSet.Config as cms
 # reference: https://github.com/cms-sw/genproductions/blob/master/python/EightTeV/SingleMuMinusFlatPt0p2To100_cff.py
 generator = cms.EDProducer("FlatRandomOneOverPtGunProducer",
     PGunParameters = cms.PSet(
-        MaxOneOverPt = cms.double(5),
-        MinOneOverPt = cms.double(0.005),
+        MaxOneOverPt = cms.double(0.5),
+        MinOneOverPt = cms.double(0.0005),
         PartID = cms.vint32(13),
-        MaxEta = cms.double(0.9),
-        MaxPhi = cms.double(1.8),
-        MinEta = cms.double(-0.2),
-        MinPhi = cms.double(0.6),
+        MaxEta = cms.double(2.2/3),
+        MaxPhi = cms.double(3.14159265359/2),
+        MinEta = cms.double(0.0),
+        MinPhi = cms.double(3.14159265359/4),
         #XFlatSpread = cms.double(1.5),  ## in mm
         #YFlatSpread = cms.double(1.5),  ## in mm
         #ZFlatSpread = cms.double(150.), ## in mm
         RandomCharge = cms.bool(True),
-        #ReallyFlat = cms.bool(True),
+        ReallyFlat = cms.bool(True),
     ),
     Verbosity = cms.untracked.int32(0),
-    psethack = cms.string('single muon+/- 1/pt 0.005 to 5 tower 27'),
+    psethack = cms.string('single muon+/- 1/pt 0.0005 to 0.5 tower 27'),
     AddAntiParticle = cms.bool(False),
     firstRun = cms.untracked.uint32(1)
 )
