8d04a4a6e32b59f77871232e7d0899541d378901
Jia Fu Low <jia.fu.low@cern.ch>
2015-04-12 00:35:20 -0500

    Updated tests

--- AMSimulation/python/test/testBankAnalysis.py
+++ AMSimulation/python/test/testBankAnalysis.py
@@ -4,26 +4,23 @@ from ROOT import TFile, TTree, gROOT, gSystem, vector
 
 
 class TestAMSim(unittest.TestCase):
-    """Test the track fitting results produced by TrackFitter"""
+    """Test the pattern attributes produced by PatternAnalyzer"""
 
-    infile = "tracks.root"
+    infile = "attribs.root"
 
     def setUp(self):
         gROOT.SetBatch(True)
         gROOT.SetMacroPath(gSystem.Getenv("CMSSW_BASE")+"/src/SLHCL1TrackTriggerSimulations/AMSimulation")
         gROOT.LoadMacro("python/test/loader.h+")
         self.tfile = TFile.Open(self.infile)
-        self.ttree = self.tfile.Get("ntupler/tree")
+        self.ttree = self.tfile.Get("patternAttributes")
         self.nevents = self.ttree.GetEntries()
 
     def tearDown(self):
         pass
 
     def test_nevents(self):
-        self.assertEqual(self.nevents, 100)
-
-    def test_ntracks(self):
-        pass
+        self.assertEqual(self.nevents, 2)
 
 
 if __name__ == "__main__":
--- AMSimulation/test/runtests_amsim.sh
+++ AMSimulation/test/runtests_amsim.sh
@@ -11,10 +11,10 @@ PYTHONTEST=${CMSSW_BASE}/src/SLHCL1TrackTriggerSimulations/AMSimulation/python/t
 (amsim -C -i test_ntuple.root -o stubs.root -n 100 --timing) || die 'Failure during stub cleaning' $?
 (python ${PYTHONTEST}/testStubCleaning.py ${LOCAL_TOP_DIR}/stubs.root) || die 'Failure using tesStubCleaning.py' $?
 
-(amsim -G -i stubs.root -o patternBank.root -n 100 --timing) || die 'Failure during pattern bank generation' $?
-#WONTFIX# (python ${PYTHONTEST}/testPatternBankGeneration.py ${LOCAL_TOP_DIR}/patternBank.root) || die 'Failure using patternBankGeneration.py' $?
+(amsim -B -i stubs.root -o patternBank.root -n 100 --timing) || die 'Failure during pattern bank generation' $?
+#WONTFIX# (python ${PYTHONTEST}/testBankGeneration.py ${LOCAL_TOP_DIR}/patternBank.root) || die 'Failure using testBankGeneration.py' $?
 
-(amsim -R -i test_ntuple.root -o roads.root -B patternBank.root -n 100 --timing) || die 'Failure during pattern recognition' $?
+(amsim -R -i test_ntuple.root -o roads.root -b patternBank.root -n 100 --timing) || die 'Failure during pattern recognition' $?
 #WONTFIX# (python ${PYTHONTEST}/testPatternRecognition.py ${LOCAL_TOP_DIR}/roads.root) || die 'Failure using testPatternRecognition.py' $?
 
 (amsim -M -i stubs.root -o matrices.txt -n 100 --timing) || die 'Failure during matrix building' $?
@@ -23,11 +23,8 @@ PYTHONTEST=${CMSSW_BASE}/src/SLHCL1TrackTriggerSimulations/AMSimulation/python/t
 (amsim -T -i roads.root -o tracks.root -m matrices.txt -n 100 --timing) || die 'Failure during track fitting' $?
 #WONTFIX# (python ${PYTHONTEST}/testTrackFitting.py ${LOCAL_TOP_DIR}/tracks.root) || die 'Failure using testTrackFitting.py' $?
 
-(amsim -A -i stubs.root -o attribs.root -B patternBank.root -n 100 --timing) || die 'Failure during pattern bank analysis' $?
+(amsim -A -i stubs.root -o attribs.root -b patternBank.root -n 100 --timing) || die 'Failure during pattern bank analysis' $?
+#WONTFIX# (python ${PYTHONTEST}/testBankAnalysis.py ${LOCAL_TOP_DIR}/tracks.root) || die 'Failure using testBankAnalysis.py' $?
 
 (amsim -W -i test_ntuple.root -o results.root --roads roads.root --tracks tracks.root -n 100 --timing) || die 'Failure during ntuple writing' $?
 #WONTFIX# (python ${PYTHONTEST}/testWriting.py ${LOCAL_TOP_DIR}/results.root) || die 'Failure using testWriting.py' $?
-
-(amconvert -B -i patternBank.root -o patternBank.txt -n 100 --format x) || die 'Failure during bank conversion' $?
-#WONTFIX# CHARCOUNT=`wc -m < "${LOCAL_TOP_DIR}/patternBank.txt"`
-#WONTFIX# [ "${CHARCOUNT}" -eq 4311 ] || die 'Failure checking the character counts' $?
